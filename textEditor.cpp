2e4103e97747f7533a9d235dfb275af291f5efed
Qihuanye <qihuanyeqhy@163.com>
2026-04-05 13:37:04 +0800

    Add basic command loop

--- textEditor.cpp
+++ textEditor.cpp
@@ -1,6 +1,25 @@
 #include "textEditor.h"
 #include <iostream>
+#include <string>
 
 void textEditor::run(){
-  std::cout << "Editor is running" << std::endl;
+  std::cout << "Simple Text Editor" << std::endl;
+  std::cout << "Type help to see commands." << std::endl;
+  std::string command;
+
+  while (true){
+    std::cout << "\n> ";
+    std::getline(std::cin, command);
+
+    if (command == "help"){
+      std::cout << "Available commands: help, quit" << std::endl;
+    } else if (command == "quit"){
+      std::cout << "Goodbye!" << std::endl;
+      break;
+    } else if (command.empty()){
+      continue;
+    } else {
+      std::cout <<  "Unknown command" << std::endl;
+    }
+  }
 }
