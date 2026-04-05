d98503756274a21e1325a8b028521b43435d28e0
Qihuanye <qihuanyeqhy@163.com>
2026-04-05 19:54:18 +0800

    add FLTK text editor widget

--- editorWindow.cpp
+++ editorWindow.cpp
@@ -1,5 +1,8 @@
 #include "editorWindow.h"
 
 EditorWindow::EditorWindow(int w, int h, const char* title) : Fl_Double_Window(w, h, title){
+  textEditor = new Fl_Text_Editor(0, 0, w, h);
+  textEditor->buffer(textBuffer);
+  resizable(textEditor);
   end();
 };
--- editorWindow.h
+++ editorWindow.h
@@ -2,10 +2,16 @@
 #define EDITOR_WINDOW_H
 
 #include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Text_Buffer.H>
+#include <FL/Fl_Text_Editor.H>
 
 class EditorWindow : public Fl_Double_Window {
 public:
   EditorWindow(int w, int h, const char* title);
+
+private:
+  Fl_Text_Editor* textEditor;
+  Fl_Text_Buffer textBuffer;
 };
 
 #endif
