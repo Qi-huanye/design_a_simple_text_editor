5aff04ad472a156b9757a7e9b6008aaca533e7a4
Qihuanye <qihuanyeqhy@163.com>
2026-04-05 14:11:23 +0800

    Add new and show commands

--- textEditor.cpp
+++ textEditor.cpp
@@ -1,25 +1,46 @@
 #include "textEditor.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
-void textEditor::run(){
+void textEditor::run() {
   std::cout << "Simple Text Editor" << std::endl;
   std::cout << "Type help to see commands." << std::endl;
   std::string command;
 
-  while (true){
+  while (true) {
     std::cout << "\n> ";
     std::getline(std::cin, command);
 
-    if (command == "help"){
-      std::cout << "Available commands: help, quit" << std::endl;
-    } else if (command == "quit"){
+    if (command == "help") {
+      std::cout << "Available commands: help, quit, show, new" << std::endl;
+    } else if (command == "quit") {
       std::cout << "Goodbye!" << std::endl;
       break;
-    } else if (command.empty()){
+    } else if (command.empty()) {
       continue;
+    } else if (command == "new") {
+      newFile();
+    } else if (command == "show") {
+      show();
     } else {
-      std::cout <<  "Unknown command" << std::endl;
+      std::cout << "Unknown command" << std::endl;
     }
+  }
+}
+
+void textEditor::newFile() {
+  file.clear();
+  std::cout << "New file created" << std::endl;
+}
+
+void textEditor::show() {
+  if (file.empty()) {
+    std::cout << "The file is empty" << std::endl;
+    return;
+  }
+
+  for (size_t i = 0; i < file.size(); i++) {
+    std::cout << i + 1 << ":" << file[i] << std::endl;
   }
 }
--- textEditor.h
+++ textEditor.h
@@ -1,9 +1,17 @@
 #ifndef TEXT_EDITOR_H
 #define TEXT_EDITOR_H
 
+#include <string>
+#include <vector>
+
 class textEditor {
 public:
   void run();
+  void newFile();
+  void show();
+
+private:
+  std::vector<std::string> file;
 };
 
 #endif
