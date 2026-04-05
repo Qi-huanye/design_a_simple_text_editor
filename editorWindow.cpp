4c3c46767584ca8898b9e8f29b39bee1298c35d5
Qihuanye <qihuanyeqhy@163.com>
2026-04-05 20:52:21 +0800

    Add file menuBar

--- editorWindow.cpp
+++ editorWindow.cpp
@@ -1,8 +1,40 @@
 #include "editorWindow.h"
 
-EditorWindow::EditorWindow(int w, int h, const char* title) : Fl_Double_Window(w, h, title){
-  textEditor = new Fl_Text_Editor(0, 0, w, h);
+EditorWindow::EditorWindow(int w, int h, const char* title) : Fl_Double_Window(w, h, title) {
+  textEditor = new Fl_Text_Editor(0, 25, w, h - 25);
   textEditor->buffer(textBuffer);
+
+  menuBar = new Fl_Menu_Bar(0, 0, this->w(), 25);
+  menuBar->add("File/Open", 0, Open, this);
+  menuBar->add("File/Save", 0, Save, this);
+
   resizable(textEditor);
   end();
-};
+}
+
+void EditorWindow::open(const char* fileName) {
+  if (!textBuffer.loadfile(fileName)) {
+    currentFileName = fileName;
+  }
+}
+
+void EditorWindow::save(const char* fileName) {
+  if (!textBuffer.savefile(fileName)) {
+    currentFileName = fileName;
+  }
+}
+
+void EditorWindow::save() {
+  if (!currentFileName.empty()) {
+    textBuffer.savefile(currentFileName.c_str());
+  }
+}
+
+void EditorWindow::Open(Fl_Widget*, void* data) {
+  EditorWindow* self = static_cast<EditorWindow*>(data);
+}
+
+void EditorWindow::Save(Fl_Widget*, void* data) {
+  EditorWindow* self = static_cast<EditorWindow*>(data);
+  self->save();
+}
--- editorWindow.h
+++ editorWindow.h
@@ -2,16 +2,25 @@
 #define EDITOR_WINDOW_H
 
 #include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Menu_Bar.H>
 #include <FL/Fl_Text_Buffer.H>
 #include <FL/Fl_Text_Editor.H>
+#include <string>
 
 class EditorWindow : public Fl_Double_Window {
 public:
   EditorWindow(int w, int h, const char* title);
 
 private:
+  void open(const char* fileName);
+  void save(const char* fileName);
+  void save();
+  static void Open(Fl_Widget*, void*);
+  static void Save(Fl_Widget*, void*);
+  Fl_Menu_Bar* menuBar;
   Fl_Text_Editor* textEditor;
   Fl_Text_Buffer textBuffer;
+  std::string currentFileName;
 };
 
 #endif
